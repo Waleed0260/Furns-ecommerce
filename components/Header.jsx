064106fe5c2b0a61b6ed5454999970a71416e336
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-03-09 12:17:10 -0800

    images

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="">
+            <Image/>
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
