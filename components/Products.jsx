7c95287b0e50ef6be3accd6adef8353ee88c1ae2
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-04-02 14:07:39 -0700

    ndlf

--- components/Products.jsx
+++ components/Products.jsx
@@ -5,11 +5,12 @@ import ProductsPage from "./ProductsPage";
 
 const Products = ({post, category, trending}) => {
 
+  const[hover, setHover] = useState(false)
+
   const [items, setItems] = useState(true);
   const[feature, setFeature] = useState(false);
   const[sale, setSale] = useState(false);
   const[trend, setTrend] = useState(false);
-  const[hover, setHover] = useState(false)
 
 
   const handleItems = ()=>{
@@ -42,19 +43,19 @@ const Products = ({post, category, trending}) => {
 
   return (
     <>
-      <div className="flex lg:justify-center md:items-center flex-col gap-5 md:mx-8 2xs:justify-center 2xs:items-center 2xs:mt-14 2xs:mr-[-180px]">
+      <div className="flex lg:justify-center md:items-center flex-col gap-5 md:mx-8 2xs:justify-center 2xs:items-center 2xs:mt-14 2xs:mr-[-30px]">
         <h1 className="text-5xl font-bold">Our Products</h1>
         <p className="md:w-[450px] text-center 2xs:w-[330px]">
           Lorem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo
           tempor incididunt ut labore
         </p>
-        <div className="flex flex-row gap-10 p-5">
+        <div className="flex flex-row xs:gap-10 p-5 2xs:gap-2">
           <b className={items ? "text-[#f97316] cursor-pointer": "text-black cursor-pointer"} onClick={handleItems}>New Arrival</b>
           <b className={feature ? "text-[#f97316] cursor-pointer": "text-black cursor-pointer"} onClick={handleFeature}>Featured</b>
           <b className={sale ? "text-[#f97316] cursor-pointer": "text-black cursor-pointer"} onClick={handleSale}>On Sale</b>
           <b className={trend ? "text-[#f97316] cursor-pointer": "text-black cursor-pointer"} onClick={handleTrend}>Trending</b>
         </div>
-        <div className="grid lg:grid-cols-4 lg:ml-14 p-6	lg:gap-16 md:grid-cols-3 md:gap-x-[11rem] md:ml-[-3rem] sm:grid-cols-2 sm:gap-x-[7rem] 2xs:ml-[-2rem] 2xs:grid-cols-1">
+        <div className="grid lg:grid-cols-4 lg:ml-14 p-6	lg:gap-16 md:grid-cols-3 md:gap-x-[11rem] md:ml-[-3rem] sm:grid-cols-2 sm:gap-x-[7rem] 2xs:ml-[-5rem] 2xs:grid-cols-1">
           {/* <ProductsPage post={post}/> */}
           {items && post.map((items, i)=>{
           const src = urlFor(items.image).url();
